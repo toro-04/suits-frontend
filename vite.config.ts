00d3fe82452395a4fd50cbb5597e363e211eb035
Toro <anmolmm04@gmail.com>
2025-09-30 13:51:22 +0530

    svg changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 important for clean routing on Vercel
   css: {
     devSourcemap: true,
   },
